14c0d873a774101c9dfa8324d4b8cf2b13572058
Ramon Wijnands <rayman747@hotmail.com>
2014-09-08 11:45:03 +0200

    simplify the code for Janno's experiment

--- app/scripts/object-detector.js
+++ app/scripts/object-detector.js
@@ -1,73 +1,16 @@
 var color_map = {};
 
 var known_objects = [
-    "milk",
-    "pringles",
-    "noodles",
-    "biscuits",
-    "water",
-    "baby_food",
-    "chocolate_milk",
-    "bread",
+    "kitchen",
+    "door",
+    "cabinet",
+    "wall",
 ];
 
-$( document ).ready(function() {
-
-    var source = $('#objects-template').html();
-    var template = Handlebars.compile(source);
-
-    var obj_list = $('#objects-list');
-
-    var detector = new ROSLIB.Topic({
-        ros : ros,
-        name : '/detected_objects',
-        messageType : 'std_msgs/String'
-    });
-    detector.subscribe(object_callback);
-
-    var trigger = new ROSLIB.Topic({
-        ros : ros,
-        name : '/trigger',
-        messageType : 'std_msgs/String'
-    });
-
-    setTimeout(function() {
-        object_callback({data:''});
-    }, 1);
-    function object_callback(msg) {
-        var objs = msg.data;
-        if (!objs) {
-            objs = [];
-        } else {
-            objs = objs.split('|');
-        }
-
-        //known_objects = _.union(known_objects, objs);
-        //known_objects.sort();
-
-        var data = known_objects.map(function (o) {
-            var c;
-            if (color_map[o]) {
-                c = color_map[o];
-            } else {
-                c = color_map[o] =  random_color();
-            }
-            return {
-                color: c, name: o, found: objs.indexOf(o)!=-1
-            };
-        });
-        //background-color
-        obj_list.html(template(data));
-    }
-
-    obj_list.on('click', 'button', function (e) {
-        var name = $(e.currentTarget).html().trim();
-        console.log('click', name);
-        trigger.publish({data:name});
-    });
-
+var random_color = (function() {
+    // this is code for generating random colors
     var golden_ratio_conjugate = 0.618033988749895;
-    var h = Math.random(); // use random start value
+    var h = 0.3; // use random start value
 
     function random_color(){
         h += golden_ratio_conjugate;
@@ -81,4 +24,45 @@ $( document ).ready(function() {
         l = Math.floor(l*100);
         return 'hsl(' + h + ',' + s + '%,' + l + '%)';
     }
+
+    return random_color;
+})();
+
+$( document ).ready(function() {
+
+    var source = $('#objects-template').html();
+    var template = Handlebars.compile(source);
+
+    var obj_list = $('#objects-list');
+
+    // generate the data for the template
+    var data = known_objects.map(function (o) {
+        var c;
+        if (color_map[o]) {
+            c = color_map[o];
+        } else {
+            c = color_map[o] = random_color();
+        }
+        return {
+            color: c, name: o
+        };
+    });
+
+    // generate the buttons
+    obj_list.html(template(data));
+
+    // this is the topic we will publish clicks to
+    var trigger = new ROSLIB.Topic({
+        ros : ros,
+        name : '/trigger',
+        messageType : 'std_msgs/String'
+    });
+
+    // publish the innerHTML of a button on that topic
+    obj_list.on('click', 'button', function (e) {
+        var name = $(e.currentTarget).html().trim();
+        console.log('click', name);
+        trigger.publish({data:name});
+    });
+
 });
