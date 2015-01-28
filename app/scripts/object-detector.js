62420bfca4167b8957ea97c14c3cd25759301532
Ramon Wijnands <rayman747@hotmail.com>
2015-01-28 12:04:10 +0100

    import newest gulp-webapp structure

--- app/scripts/object-detector.js
+++ app/scripts/object-detector.js
@@ -1,73 +1,73 @@
 'use strict';
 
 var random_color = (function() {
-    // this is code for generating random colors
-    var golden_ratio_conjugate = 0.618033988749895;
-    var h = 0.3; // use random start value
+  // this is code for generating random colors
+  var golden_ratio_conjugate = 0.618033988749895;
+  var h = 0.3; // use random start value
 
-    function random_color(){
-        h += golden_ratio_conjugate;
-        h %= 1;
-        return get_color(h, 0.7, 0.8);
-    }
+  function random_color(){
+    h += golden_ratio_conjugate;
+    h %= 1;
+    return get_color(h, 0.7, 0.8);
+  }
 
-    function get_color(h, s, l) {
-        h = Math.floor(h*360);
-        s = Math.floor(s*100);
-        l = Math.floor(l*100);
-        return 'hsl(' + h + ',' + s + '%,' + l + '%)';
-    }
+  function get_color(h, s, l) {
+    h = Math.floor(h*360);
+    s = Math.floor(s*100);
+    l = Math.floor(l*100);
+    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+  }
 
-    return random_color;
+  return random_color;
 })();
 
 $( document ).ready(function() {
 
-    var source = $('#objects-template').html();
-    var template = Handlebars.compile(source);
+  var source = $('#objects-template').html();
+  var template = Handlebars.compile(source);
 
-    var obj_list = $('#objects-list');
+  var obj_list = $('#objects-list');
 
-    // receive list of locations
-    var inTopic = new ROSLIB.Topic({
-        ros : ros,
-        name : '/locations_list',
-        messageType : 'std_msgs/String'
+  // receive list of locations
+  var inTopic = new ROSLIB.Topic({
+    ros : ros,
+    name : '/locations_list',
+    messageType : 'std_msgs/String'
+  });
+
+  inTopic.subscribe(function (msg) {
+    console.log(msg);
+  });
+
+  // this is the topic we will publish clicks to
+  var outTopic = new ROSLIB.Topic({
+    ros : ros,
+    name : '/nav_goal',
+    messageType : 'std_msgs/String'
+  });
+
+  // publish the innerHTML of a button on that topic
+  obj_list.on('click', 'button', function (e) {
+    var name = $(e.currentTarget).html().trim();
+    console.log('click', name);
+    outTopic.publish({data:name});
+  });
+
+  obj_list.html(template({
+     status: 'loading'
+  }));
+
+  // generate the data for the template
+  function generateButtonData(locations) {
+    return locations.map(function (o) {
+      var c = random_color();
+      return {
+        color: c, name: o
+      };
     });
+  }
 
-    inTopic.subscribe(function (msg) {
-        console.log(msg);
-    });
-
-    // this is the topic we will publish clicks to
-    var outTopic = new ROSLIB.Topic({
-        ros : ros,
-        name : '/nav_goal',
-        messageType : 'std_msgs/String'
-    });
-
-    // publish the innerHTML of a button on that topic
-    obj_list.on('click', 'button', function (e) {
-        var name = $(e.currentTarget).html().trim();
-        console.log('click', name);
-        outTopic.publish({data:name});
-    });
-
-    obj_list.html(template({
-       status: 'loading'
-    }));
-
-    // generate the data for the template
-    function generateButtonData(locations) {
-        return locations.map(function (o) {
-            var c = random_color();
-            return {
-                color: c, name: o
-            };
-        });
-    }
-
-    var locations = ['asdf', 'omg']
-    var data = generateButtonData(locations);
-    obj_list.html(template(data));
+  var locations = ['asdf', 'omg']
+  var data = generateButtonData(locations);
+  obj_list.html(template(data));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,114 @@
+/* jshint node:true */
 'use strict';
-// generated on 2014-09-08 using generator-gulp-webapp 0.1.0
-
+// generated on 2015-01-28 using generator-gulp-webapp 0.2.0
 var gulp = require('gulp');
-
-// load plugins
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-    return gulp.src('app/styles/main.css')
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
+  return gulp.src('app/styles/main.css')
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe(gulp.dest('.tmp/styles'));
 });
 
-gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size());
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'));
+    // .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('html', ['styles', 'scripts'], function () {
-    var jsFilter = $.filter('**/*.js');
-    var cssFilter = $.filter('**/*.css');
+gulp.task('html', ['styles'], function () {
+  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
-    return gulp.src('app/*.html')
-        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
-        .pipe(jsFilter)
-        // .pipe($.uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function () {
-    return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('fonts', function () {
-    return $.bowerFiles()
-        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-        .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size());
+  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
-        .pipe(gulp.dest('dist'));
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html',
+    'node_modules/apache-server-configs/dist/.htaccess'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
-});
-
-gulp.task('build', ['html', 'images', 'fonts', 'extras']);
-
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('connect', function () {
-    var connect = require('connect');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.static('.tmp'))
-        .use(connect.directory('app'));
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
+    .use(require('connect-livereload')({port: 35729}))
+    .use(serveStatic('.tmp'))
+    .use(serveStatic('app'))
+    // paths to bower_components should be relative to the current file
+    // e.g. in app/index.html you should use ../bower_components
+    .use('/bower_components', serveStatic('bower_components'))
+    .use(serveIndex('app'));
 
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
-        });
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
 });
 
-gulp.task('serve', ['connect'], function () {
-    require('opn')('http://localhost:9000');
+gulp.task('serve', ['connect', 'watch'], function () {
+  require('opn')('http://localhost:9000');
 });
 
 // inject bower components
 gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
+  var wiredep = require('wiredep').stream;
 
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app'));
+  gulp.src('app/*.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app'));
 });
 
-gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
+gulp.task('watch', ['connect'], function () {
+  $.livereload.listen();
 
-    // watch for changes
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    '.tmp/styles/**/*.css',
+    'app/scripts/**/*.js',
+    'app/images/**/*'
+  ]).on('change', $.livereload.changed);
 
-    gulp.watch([
-        'app/*.html',
-        '.tmp/styles/**/*.css',
-        'app/scripts/**/*.js',
-        'app/images/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
+  gulp.watch('app/styles/**/*.css', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
 
-    gulp.watch('app/styles/**/*.css', ['styles']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
-    gulp.watch('app/images/**/*', ['images']);
-    gulp.watch('bower.json', ['wiredep']);
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
 });
